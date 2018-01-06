56156f333c3ad6b84b65b54f93e136eb5deeab58
Michael Williamson <mike.a.williamson@gmail.com>
2018-01-06 11:39:24 -0500

    Introduce Interface Association Descriptor and add it to the config structure.

--- libusb/libusb.h
+++ libusb/libusb.h
@@ -269,6 +269,9 @@ enum libusb_descriptor_type {
 	/** Endpoint descriptor. See libusb_endpoint_descriptor. */
 	LIBUSB_DT_ENDPOINT = 0x05,
 
+	/** Interface Association Descriptor. */
+	LIBUSB_DT_IAD = 0x0b,
+
 	/** BOS descriptor */
 	LIBUSB_DT_BOS = 0x0f,
 
@@ -643,6 +646,17 @@ struct libusb_interface {
 	int num_altsetting;
 };
 
+struct libusb_iad_descriptor {
+	uint8_t  bLength;
+	uint8_t  bDescriptorType;
+	uint8_t  bFirstInterface;
+	uint8_t  bInterfaceCount;
+	uint8_t  bFunctionClass;
+	uint8_t  bFunctionSubClass;
+	uint8_t  bFunctionProtocol;
+	uint8_t  iFunction;
+}; // add to configuration
+
 /** \ingroup libusb_desc
  * A structure representing the standard USB configuration descriptor. This
  * descriptor is documented in section 9.6.3 of the USB 3.0 specification.
@@ -688,6 +702,8 @@ struct libusb_config_descriptor {
 
 	/** Length of the extra descriptors, in bytes. */
 	int extra_length;
+
+	struct libusb_iad_descriptor iad;
 };
 
 /** \ingroup libusb_desc
